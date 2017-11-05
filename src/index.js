d6aed9ee208f284a3f0ff24e157aa0ed53da9bab
stackbaxter <stackbaxter@yahoo.com>
2017-11-05 07:18:51 -0600

    synch

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
 import testRoutes from "./routes/testRoutes";
-
+// "mongodb://stack:stack@ds241895.mlab.com:41895/entrance"
 mongoose.set("debug", true);
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://stack:stack@ds241895.mlab.com:41895/entrance");
+mongoose.connect("mongodb://stack:stack@ds139904.mlab.com:39904/catfork");
 const app = express();
 app.use(bodyParser.json());
 app.use(testRoutes);
