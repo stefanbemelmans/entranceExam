89a1f024df833fcc9af33aa774cafad6a6a1c654
stackbaxter <stackbaxter@gmail.com>
2017-11-05 06:37:14 -0600

    edits

--- client/src/components/EntranceExam.js
+++ client/src/components/EntranceExam.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 // import AddIngredients from "./AddIngredients";
-// import {withRouter} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import PropTypes from "prop-types";
 import TestForm from "./TestForm";
 
--- client/src/components/TestForm.js
+++ client/src/components/TestForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-// import MealTypes from "./MealTypes";
-// import {withRouter} from "react-router-dom";
+//import MealTypes from "./MealTypes";
+import {withRouter} from "react-router-dom";
 
 export default class TestForm extends Component {
   constructor(props) {
